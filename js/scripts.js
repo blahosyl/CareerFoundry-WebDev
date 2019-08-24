77e63e598d4bd6f853070ef8677edd78921a30e3
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-08-24 17:21:43 +0200

    2.8: Conditional Statements pt. 1
    - add conditionals to form text box:
        - only display "Thank you" message and hide form if message box is not empty
        - show nr. of characters in text box, change colour after a certain character limit
    
    Problem: weird JS error (on hover to text box): "Uncaught (in promise)

--- js/scripts.js
+++ js/scripts.js
@@ -25,17 +25,39 @@ $(document).ready(function(){
     var comment = $('.message-box').val();
     var name = $('#form-name').val();
     var email = $('#form-email').val();
-    $('#visible-comment').html("Thank you for your message! You submitted the following:<br>Name: "+name+"<br>Email: "+email+"<br>Message: "+comment).addClass("form-submitted");
-    $('#form-textbox').hide();
-    $('#form-email').hide();
-    $('#form-name').hide();
-    $('#form-btn').hide();
-    return false;
+    // var charCount = $(".message-box").val().length;
+    if (comment === "") {
+      $('.message-box').css("border-color", "red");
+    } else {
+      $('#visible-comment').html("Thank you for your message! You submitted the following:<br>Name: "+name+"<br>Email: "+email+"<br>Message: "+comment).addClass("form-submitted");
+      $('#form-textbox').hide();
+      $('#form-email').hide();
+      $('#form-name').hide();
+      $('#form-btn').hide();
+      // $('.message-box').css("border-color", "green");
+      return false;
+    };
   });
+
+  //Text box to change background colour on hover
   $('#form-textbox') .hover(
     function() {
       $(this).css("background-color", "var(--quite-light)");
-      }, function(){
+    }, function(){
       $(this).css("background-color", "white");
     });
+
+  //track key strokes in message box
+  $(".message-box").on("keyup", function() {
+    //set the length of the content of the textarea to a variable
+    //show a running character count to the user
+    var charCount = $(".message-box").val().length;
+    $("#char-count").html("Nr. of characters: "+charCount);
+    if(charCount > 10) {
+      $("#char-count").css("color", "red");
+    } else {
+      $("#char-count").css("color", "white");
+    };
+  });
+
 });
