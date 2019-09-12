49108707bbb075a2c2a5f36855c8acac0aaf258d
Sylvia Blaho <sylvia.blaho@gmail.com>
2019-09-12 23:46:08 +0200

    2.10: Object Literals
    - change list of portfolio items to object (from array)
    - add text on mouseover to portfolio pictures
    - add URL to portfolio pictures
    - change  opacity of to portfolio pictures and colour of text for better legibility

--- js/work.js
+++ js/work.js
@@ -1,6 +1,22 @@
 var works = [
-  'img/poodle.png',
-  'img/circles.gif',
-  'img/sp-sylvia.jpg',
-  'img/sp-sylvia.jpg'
-]
+  {
+    title: "Freedom of Analysis",
+    pic: "img/sp-sylvia.jpg",
+    url: "https://www.degruyter.com/view/product/178827"
+  },
+  {
+    title: "The Syntax of Phonology",
+    pic: "img/uit.png",
+    url: "http://site.uit.no/castl/research/output/phd-dissertations/#Blaho"
+  },
+  {
+    title: "Modeling Ungramma- ticality",
+    pic: "img/sp-sylvia urban.jpg",
+    url: "https://www.equinoxpub.com/home/modeling-ungrammaticality-optimality-theory-curt-rice-slyvia-blaho/"
+  },
+  {
+    title: "Hungarian Academy of Sciences",
+    pic: "img/marie_curie_logo.jpg",
+    url: "https://www.researchgate.net/scientific-contributions/81448823_Sylvia_Blaho"
+  }
+];
